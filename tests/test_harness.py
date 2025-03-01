6602178b039926c4c65da6d216f45d832fc78a1c
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-03-01 19:30:37 +1100

    Add harness tests

--- tests/test_harness.py
+++ tests/test_harness.py
@@ -32,6 +32,42 @@ class TestHarness(unittest.TestCase):
         )
         harness_path = create_harness(
             initial_state,
+            output_harness_name=self.temp_path / "test_harness.asm",
+        )
+        self.assertTrue(harness_path.exists())
+        with open(harness_path, "r") as f:
+            content = f.read()
+            self.assertIn("li $a0, 10", content)
+            self.assertIn("li $v0, 20", content)
+            self.assertIn("li $t0, 30", content)
+            self.assertIn("sw $t0, ($t1)", content)
+            self.assertIn("j main", content)
+
+    def test_create_harness_file_with_dict(self):
+        initial_state = {
+            "registers": {"a0": "10", "v0": "20"},
+            "memory": {"1000": "30", "1004": "40"},
+        }
+        harness_path = create_harness(
+            initial_state,
+            output_harness_name=self.temp_path / "test_harness.asm",
+        )
+        self.assertTrue(harness_path.exists())
+        with open(harness_path, "r") as f:
+            content = f.read()
+            self.assertIn("li $a0, 10", content)
+            self.assertIn("li $v0, 20", content)
+            self.assertIn("li $t0, 30", content)
+            self.assertIn("sw $t0, ($t1)", content)
+            self.assertIn("j main", content)
+
+    def test_create_harness_with_custom_label(self):
+        initial_state = MipsState(
+            registers={"a0": "10", "v0": "20"},
+            memory={"1000": "30", "1004": "40"},
+        )
+        harness_path = create_harness(
+            initial_state,
             label="start",
             output_harness_name=self.temp_path / "test_harness.asm",
         )
@@ -43,6 +79,42 @@ class TestHarness(unittest.TestCase):
             self.assertIn("li $t0, 30", content)
             self.assertIn("sw $t0, ($t1)", content)
             self.assertIn("j start", content)
+
+    def test_create_harness_jal(self):
+        initial_state = MipsState(
+            registers={"a0": "10", "v0": "20"},
+            memory={"1000": "30", "1004": "40"},
+        )
+        # test 1 with default label:
+        harness_path = create_harness(
+            initial_state,
+            output_harness_name=self.temp_path / "test_harness.asm",
+            jump_type=JumpType.JUMP_AND_LINK,
+        )
+        self.assertTrue(harness_path.exists())
+        with open(harness_path, "r") as f:
+            content = f.read()
+            self.assertIn("li $a0, 10", content)
+            self.assertIn("li $v0, 20", content)
+            self.assertIn("li $t0, 30", content)
+            self.assertIn("sw $t0, ($t1)", content)
+            self.assertIn("jal main", content)
+
+        # test 2 with custom label:
+        harness_path = create_harness(
+            initial_state,
+            label="start",
+            output_harness_name=self.temp_path / "test_harness.asm",
+            jump_type=JumpType.JUMP_AND_LINK,
+        )
+        self.assertTrue(harness_path.exists())
+        with open(harness_path, "r") as f:
+            content = f.read()
+            self.assertIn("li $a0, 10", content)
+            self.assertIn("li $v0, 20", content)
+            self.assertIn("li $t0, 30", content)
+            self.assertIn("sw $t0, ($t1)", content)
+            self.assertIn("jal start", content)
 
 
 if __name__ == "__main__":
