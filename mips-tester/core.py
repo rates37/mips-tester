f677ea1a95d25a654f2f5d356c609e6ee05b97b5
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-03-01 18:33:41 +1100

    Add core configs and data models

---
+++ mips-tester/core.py
@@ -0,0 +1,55 @@
+"""
+Contains core functionality for the MIPS testing library.
+Includes configuration and shared utils
+"""
+
+from pydantic import BaseModel, Field
+from typing import Optional
+from pathlib import Path
+
+
+class MipsConfig(BaseModel):
+    """
+    Configuration for the MIPS tester environment.
+    """
+
+    mars_path: Path = Field(
+        default=Path("mars.jar"),
+        description="Path to the MIPS mars jar executable file",
+    )
+    default_max_steps: int = Field(
+        default=10000,
+        description="Default maximum number of steps to simulate MIPS programs for",
+    )
+    default_output_harness: str = Field(
+        deafult="harness.asm", description="Default output filename for test harness"
+    )
+
+    class Config:
+        validate_assignment = True
+
+
+# Global config instance:
+config = MipsConfig()
+
+
+def configure(
+    mars_path: str | None, max_steps: int | None, output_harness: str | None
+) -> None:
+    """Updates global config for MIPS tester
+
+    Args:
+        mars_path (str | None): Path to Mars JAR file
+        max_steps (int | None): Default maximum number of steps to simulate for
+        output_harness (str | None): Default harness filename
+    """
+    global config
+
+    if mars_path is not None:
+        config.mars_path = Path(mars_path)
+
+    if max_steps is not None:
+        config.default_max_steps = max_steps
+
+    if output_harness is not None:
+        config.default_output_harness = output_harness
---
+++ mips-tester/models.py
@@ -0,0 +1,99 @@
+"""
+Defined data models for MIPS testing library
+"""
+
+from enum import Enum
+from pydantic import BaseModel, Field, validator
+
+
+class JumpType(str, Enum):
+    """Type of jump instruction to use in harness."""
+
+    JUMP = "j"
+    JUMP_AND_LINK = "jal"
+
+
+class RegisterState(BaseModel):
+    """Represents the state of MIPS registers"""
+
+    v0: str | None = None
+    v1: str | None = None
+    a0: str | None = None
+    a1: str | None = None
+    a2: str | None = None
+    a3: str | None = None
+    t0: str | None = None
+    t1: str | None = None
+    t2: str | None = None
+    t3: str | None = None
+    t4: str | None = None
+    t5: str | None = None
+    t6: str | None = None
+    t7: str | None = None
+    t8: str | None = None
+    t9: str | None = None
+    s0: str | None = None
+    s1: str | None = None
+    s2: str | None = None
+    s3: str | None = None
+    s4: str | None = None
+    s5: str | None = None
+    s6: str | None = None
+    s7: str | None = None
+
+    class Config:
+        validate_assignment = True
+
+
+class MipsState(BaseModel):
+    """Represents the complete state of a MIPS program (registers+memory)."""
+
+    registers: RegisterState = Field(default_factory=RegisterState)
+    memory: dict[str, str] = Field(
+        default_factory=dict,
+        description="Memory address to value mappings (decimal or Hex)",
+    )
+
+    @validator("memory")
+    def validate_memory_addresses(cls, v):
+        memory = dict()
+        for addr, val in v.items():
+            # ensure address is string:
+            addr_str = str(addr)
+
+            # ensure address is number (decimal or hex):
+            try:
+                int(addr_str, 0)
+            except ValueError:
+                raise ValueError(
+                    f"Invalid memory address: {addr_str}! Must be decimal or hex"
+                )
+
+            # ensure val is a string:
+            val_str = str(val)
+
+            # ensure value is number (decimal or hex):
+            try:
+                int(val_str, 0)
+            except ValueError:
+                raise ValueError(
+                    f"Invalid memory value: {val_str}! Must be decimal or hex"
+                )
+            memory[addr_str] = val_str
+        return memory
+
+    class Config:
+        validate_assignment = True
+
+
+class TestResult(BaseModel):
+    """The results of running a MIPS test."""
+
+    success: bool
+    score: float = Field(
+        default=0.0, ge=0.1, le=1.0, description="Proportion of passed checks"
+    )
+    messages: list[str] = Field(
+        default_factory=list,
+        description="List of feedback messages from running checks",
+    )
