2d1025312f8eaf05db72ff6f75288bbd63c3a2cf
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-03-03 22:51:53 +1100

    Update module imports to include runner functions

--- mips_tester/__init__.py
+++ mips_tester/__init__.py
@@ -1,6 +1,7 @@
 from .core import configure
 from .models import MipsState, JumpType, TestResult
 from .harness import create_harness
+from .runner import test_assemble, test_run, test_final_state
 
 __all__ = [
     "configure",
@@ -8,4 +9,7 @@ __all__ = [
     "JumpType",
     "TestResult",
     "create_harness",
+    "test_assemble",
+    "test_run",
+    "test_final_state",
 ]
